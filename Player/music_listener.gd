912502486f41545a44692c21fb091f79daba711d
Mastoast <dgekec@gmail.com>
2025-04-05 16:57:57 +0200

    Add music listener

---
+++ Player/music_listener.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+signal move_ship
+signal emit_little_sonar
+signal emit_big_sonar
+
+@export var player_dash: int = 10
+@export var player_big_dash: int = 20
+
+func _ready() -> void:
+	MusicPlayer.new_beat.connect(on_beat)
+
+func on_beat(beat_count:int):
+	if beat_count % 4 == 0:
+		move_ship.emit(player_big_dash)
+		emit_little_sonar.emit()
+	else:
+		move_ship.emit(player_dash)
