95edff5a35945205cd4a803829d30457e804edac
Leonis <pro@leonardvuillot.com>
2025-04-05 17:15:12 +0200

    [Dev] Ongoing player dash changes

--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 #region Variables
 @export
-var baseDashForce : float = 100
+var baseDashForce : float = 1000
 
 enum State {Idle, Scan, Dash}
 var Player_State : State = State.Idle
@@ -18,16 +18,22 @@ func _process(_delta: float) -> void:
 		look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("ui_select", true): ## dash debug
 		Player_State = State.Dash
-		latestPosition = position
-		Player_State = State.Idle
+		_dash()
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Player_State == State.Dash:
-		_dash(delta)
+		if test_move(transform, Vector2.ZERO):
+			Player_State = State.Idle
+			position = latestPosition
 
 func _dash(dashForceMultiplier: float = 1) -> void:
 	latestPosition = position
-	var col = move_and_collide(baseDashForce * dashForceMultiplier * Vector2.from_angle(rotation))
-	if col:
-		Player_State = State.Idle
-		position = latestPosition
+	var tween = create_tween()
+	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	var targetPosition = position + baseDashForce * dashForceMultiplier * Vector2.from_angle(rotation)
+	tween.tween_property(self, "position", targetPosition, 1).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
+	tween.finished.connect(_onDashFinished)
+	tween.play()
+
+func _onDashFinished() -> void:
+	Player_State = State.Idle
