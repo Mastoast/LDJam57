d3bf40c6c812aaca715e7a1ec6057d7cc01b3bdc
Leonis <pro@leonardvuillot.com>
2025-04-05 15:42:38 +0200

    [Dev] Player Dash (Space bar)

--- Environment/poly_shape.gd
+++ Environment/poly_shape.gd
@@ -1,10 +1,8 @@
 extends Polygon2D
 class_name _PolyShape
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-		(get_node("StaticBody2D/CollisionPolygon2D") as CollisionPolygon2D).polygon = polygon.duplicate()
+	(get_node("StaticBody2D/CollisionPolygon2D") as CollisionPolygon2D).polygon = polygon.duplicate()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
--- Player/player.gd
+++ Player/player.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+@export
+var baseDashForce : float = 10
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("ui_select"):
+		_dash()
+
+func _dash(dashForceMultiplier: float = 1) -> void:
+	global_position += baseDashForce * dashForceMultiplier * Vector2.from_angle(rotation)
