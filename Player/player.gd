dcaf038144f84b33daeb60d78bd400d4145dd338
Leonis <pro@leonardvuillot.com>
2025-04-05 16:26:54 +0200

    [Dev] New Player

--- Player/player.gd
+++ Player/player.gd
@@ -1,15 +1,33 @@
-extends Node2D
+extends CharacterBody2D
 
+#region Variables
 @export
-var baseDashForce : float = 10
+var baseDashForce : float = 100
+
+enum State {Idle, Scan, Dash}
+var Player_State : State = State.Idle
+
+var latestPosition : Vector2
+#endregion
 
 func _ready() -> void:
-	pass # Replace with function body.
+	latestPosition = position
 
 func _process(_delta: float) -> void:
-	look_at(get_global_mouse_position())
-	if Input.is_action_pressed("ui_select"):
-		_dash()
+	if (Player_State == State.Idle):
+		look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("ui_select", true): ## dash debug
+		Player_State = State.Dash
+		latestPosition = position
+		Player_State = State.Idle
+
+func _physics_process(delta: float) -> void:
+	if Player_State == State.Dash:
+		_dash(delta)
 
 func _dash(dashForceMultiplier: float = 1) -> void:
-	global_position += baseDashForce * dashForceMultiplier * Vector2.from_angle(rotation)
+	latestPosition = position
+	var col = move_and_collide(baseDashForce * dashForceMultiplier * Vector2.from_angle(rotation))
+	if col:
+		Player_State = State.Idle
+		position = latestPosition
