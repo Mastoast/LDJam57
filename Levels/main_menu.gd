9f74e1e38379fd405b874d79b8245f1cb57b00f9
Mastoast <dgekec@gmail.com>
2025-04-07 16:12:26 +0200

    Add menu animation

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -3,9 +3,13 @@ extends Node2D
 var game_level_path = "res://Levels/base_level.tscn"
 var fade_delay = 2.0
 
+func _ready() -> void:
+	$AnimationPlayer.play("submarine_float")
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		var tween = get_tree().create_tween()
+		tween.set_trans(Tween.TRANS_CUBIC)
 		tween.tween_property(self, "modulate", Color(1,1,1,0), fade_delay)
 		tween.tween_callback(load_level)
 
