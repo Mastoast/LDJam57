6fc50688c5e3f36cf9f08c48654f8a0414fcfac9
Mastoast <dgekec@gmail.com>
2025-04-07 17:06:11 +0200

    Add voices bus

--- Player/music_listener.gd
+++ Player/music_listener.gd
@@ -11,10 +11,21 @@ func _ready() -> void:
 	MusicPlayer.new_beat.connect(on_beat)
 
 func on_beat(beat_count:int):
+	on_beat_lvl1(beat_count)
+
+func on_beat_lvl1(beat_count:int):
 	if beat_count % 4 == 0:
 		move_ship.emit(player_big_dash, MusicPlayer.beat_length)
 		emit_big_sonar.emit()
 	elif beat_count % 4 == 1:
 		emit_big_sonar.emit()
 	else:
+		move_ship.emit(player_dash, MusicPlayer.beat_length)
+
+func on_beat_lvl2(beat_count:int):
+	if beat_count % 4 == 1:
+		emit_big_sonar.emit()
+	elif beat_count % 4 == 2:
+		emit_big_sonar.emit()
+	elif beat_count % 4 == 0:
 		move_ship.emit(player_dash, MusicPlayer.beat_length)
